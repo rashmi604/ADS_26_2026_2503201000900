ff5d7500e19b1c13b27562c5fc007b81fe4eb551
Rashmi Rajput <rjay3261@gmail.com>
2026-09-16 09:04:41 +0530

    merge sort

---
+++ Merge Sort.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int low, int mid, int high)
+{
+    int i = low;
+    int j = mid + 1;
+    int k = 0;
+
+    int temp[100];
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] < arr[j])
+        {
+            temp[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            temp[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i <= mid)
+    {
+        temp[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    while (j <= high)
+    {
+        temp[k] = arr[j];
+        j++;
+        k++;
+    }
+
+    for (i = low, k = 0; i <= high; i++, k++)
+    {
+        arr[i] = temp[k];
+    }
+}
+
+void mergeSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int mid = (low + high) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+
+        merge(arr, low, mid, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {38, 12, 27, 43, 9, 31, 18, 25};
+    int n = 8;
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
